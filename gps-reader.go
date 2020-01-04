d57827c13b836bc472e09a84c357e395859dee6f
Dave Smith-Uchida <dave@igeekinc.com>
2020-01-03 16:05:37 -0800

    Added output

--- gps-reader.go
+++ gps-reader.go
@@ -6,11 +6,12 @@ import (
 	"github.com/adrianmo/go-nmea"
 	"github.com/jacobsa/go-serial/serial"
 )
+
 type GPSReader struct {
-	port string
-	baudRate uint
-	dataBits uint
-	stopBits uint
+	port        string
+	baudRate    uint
+	dataBits    uint
+	stopBits    uint
 	keepRunning bool
 }
 
@@ -46,7 +47,17 @@ func (this GPSReader) UpdateFromGPSLoop() (err error) {
 			fmt.Printf("Raw sentence: %v\n", gpsLine)
 			sentence, err = nmea.Parse(gpsLine)
 			if sentence.DataType() == nmea.TypeRMC {
-
+				m := sentence.(nmea.RMC)
+				fmt.Printf("Time: %s\n", m.Time)
+				fmt.Printf("Validity: %s\n", m.Validity)
+				fmt.Printf("Latitude GPS: %s\n", nmea.FormatGPS(m.Latitude))
+				fmt.Printf("Latitude DMS: %s\n", nmea.FormatDMS(m.Latitude))
+				fmt.Printf("Longitude GPS: %s\n", nmea.FormatGPS(m.Longitude))
+				fmt.Printf("Longitude DMS: %s\n", nmea.FormatDMS(m.Longitude))
+				fmt.Printf("Speed: %f\n", m.Speed)
+				fmt.Printf("Course: %f\n", m.Course)
+				fmt.Printf("Date: %s\n", m.Date)
+				fmt.Printf("Variation: %f\n", m.Variation)
 			}
 		}
 	}
