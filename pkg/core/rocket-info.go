69986968c66574bb5e11bda5157ab09405c7e380
Dave Smith-Uchida <dave@igeekinc.com>
2022-07-14 22:55:46 -0700

    Reformatted and added acceleration to RocketInfoJSON

--- pkg/core/rocket-info.go
+++ pkg/core/rocket-info.go
@@ -3,14 +3,16 @@ package core
 import (
 	"encoding/json"
 	"github.com/adrianmo/go-nmea"
+	"periph.io/x/conn/v3/physic"
 	"sync"
 )
 
 type RocketInfoJSON struct {
-	GPS       nmea.GGA
-	Altitude  float64 // Altitude in meters
-	Recording bool
-	VideoFile string
+	GPS              nmea.GGA
+	Altitude         float64 // Altitude in meters
+	XAcc, YAcc, ZAcc physic.Acceleration
+	Recording        bool
+	VideoFile        string
 }
 type RocketInfo struct {
 	RocketInfoJSON
@@ -25,6 +27,12 @@ func (this *RocketInfo) UpdateGPS(gps nmea.GGA) {
 
 func (this *RocketInfo) UpdateAltitude(temperature float64, pressure float64, altitude float64) {
 	this.Altitude = altitude
+}
+
+func (this *RocketInfo) UpdateAcceleration(x, y, z physic.Acceleration) {
+	this.XAcc = x
+	this.YAcc = y
+	this.ZAcc = z
 }
 
 func (this *RocketInfo) SetRecording(recording bool, videoFile string) {
